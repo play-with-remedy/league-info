466fee5ec09d81c560ae7ecfaa0759756ab7bb54
Nick Ezhenkov <ezhenkovnickolay@gmail.com>
2019-06-03 16:22:32 +0300

    x-maf tournaments is auto up-to-dated now

--- x-maf_league/scripts/common.js
+++ x-maf_league/scripts/common.js
@@ -3,6 +3,20 @@ function switchTournaments() {
     // implement JQuery
 }
 window.onload = function () {
+    var tournaments = $('table tr');
+    $.each(tournaments, function(index, tournament) {
+        var dateString = $(tournament).find('.date').text();
+        var dsplit = dateString.split(".");
+        var date = new Date(dsplit[2], dsplit[1]-1, dsplit[0]);
+        var curDate = new Date();
+
+        if (Date.parse(curDate) >= Date.parse(date)){
+            $(tournament).addClass('old_date');
+        } else {
+            $(tournament).addClass('future');
+        }
+    });
+
     $('#sceduleOrdering a').on("click", function () {
         var href = $(this).attr('href');
         if ('#planned' === href) {
