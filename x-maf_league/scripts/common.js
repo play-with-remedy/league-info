8acb4fa9501ebaac6cabd218efe198755e7b06da
Nick Ezhenkov <ezhenkovnickolay@gmail.com>
2019-06-07 14:45:09 +0300

    [XML] Scedule table view was updated

--- x-maf_league/scripts/common.js
+++ x-maf_league/scripts/common.js
@@ -3,9 +3,9 @@ function switchTournaments() {
     // implement JQuery
 }
 window.onload = function () {
-    var tournaments = $('table tr');
+    var tournaments = $('.tournament');
     $.each(tournaments, function(index, tournament) {
-        var dateString = $(tournament).find('.date').text();
+        var dateString = $(tournament).find('.tournament_date').text();
         var dsplit = dateString.split(".");
         var date = new Date(dsplit[2], dsplit[1]-1, dsplit[0]);
         var curDate = new Date();
@@ -21,11 +21,11 @@ window.onload = function () {
         var href = $(this).attr('href');
         if ('#planned' === href) {
             $('.old_date').hide();
-            $('.future').show();
+            $('.future').css('display', 'flex');
             $('#planned_tournaments').addClass('active');
             $('#overpast_tournaments').removeClass();
         } else {
-            $('.old_date').show();
+            $('.old_date').css('display', 'flex');
             $('.future').hide();
             $('#overpast_tournaments').addClass('active');
             $('#planned_tournaments').removeClass();
