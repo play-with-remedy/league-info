a06d8436cf68077f24dec9c234e21f49d455cac4
Nick Ezhenkov <ezhenkovnickolay@gmail.com>
2019-07-14 10:49:37 +0300

    [XML] Antihype results + link color fix

--- x-maf_league/scripts/full-table.js
+++ x-maf_league/scripts/full-table.js
@@ -1,4 +1,4 @@
-const full_table_request_URL = "https://x-maf-league.github.io/league-info/x-maf_league/files/full-table.json";
+const full_table_request_URL = "https://x-maf-league.github.io/league-info/x-maf_league/files/full_table.json";
 
 window.onload = function () {
     
@@ -8,6 +8,29 @@ window.onload = function () {
     request.send();
     
     request.onload = function() {
-        alert(request.response);
+        var response = request.response;
     };
+}
+
+function create_table_header() {
+    return "<tr>" +
+        "<td>Player</td>" +
+        "<td>Games</td>" +
+        "<td>Wins</td>" +
+        "<td>Additional poiunts</td>" +
+        "<td>Penalty points</td>" +
+        "<td>Sheriff games</td>" +
+        "<td>Sheriff wins</td>" +
+        "<td>Don games</td>" +
+        "<td>Don wins</td>" +
+        "<td>Mafia games</td>" +
+        "<td>Mafia wins</td>" +
+        "<td>Citizen games</td>" +
+        "<td>Citizen wins</td>" +
+        "<td>First kill</td>" +
+        "<td>Best move</td>" +
+        "<td>Scores</td>" +
+        "<td id='total_score'>Total</td>" +
+        "<td>Percent</td>" +
+    "</tr>";
 }
