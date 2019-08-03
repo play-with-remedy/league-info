e6aea59359b042b1ac8b4a02daf004811261ae78
Nick Ezhenkov <ezhenkovnickolay@gmail.com>
2019-08-04 01:35:53 +0300

    [M] default table was added

---
+++ timetable/scripts/table.js
@@ -0,0 +1,191 @@
+var table_json = {
+    "teams" : [
+        {
+            name : "83",
+            players : [
+                {
+                    name : "Remedy",
+                    point : 2,
+                    extra : 0, 
+                    Ci : 1,
+                    win : 0
+                },
+                {
+                    name : "Yesterday",
+                    point : 1,
+                    extra : 0.2, 
+                    Ci : 0
+                }
+            ]
+        },
+        {
+            name : "Рик и Морти",
+            players : [
+                {
+                    name : "Ко-ко-ко",
+                    point : 3,
+                    extra : 0.2, 
+                    Ci : 0
+                },
+                {
+                    name : "Шпагат",
+                    point : 2,
+                    extra : 0.2, 
+                    Ci : 0
+                }
+            ]
+        },
+        {
+            name : "Молния Гендальфа",
+            players : [
+                {
+                    name : "Флэш",
+                    point : 2,
+                    extra : -0.05, 
+                    Ci : 2,
+                    win : 0/2
+                },
+                {
+                    name : "Маг",
+                    point : 3,
+                    extra : 0.45, 
+                    Ci : 1,
+                    win : 1
+                }
+            ]
+        },
+        {
+            name : "Малышка и Карлсон",
+            players : [
+                {
+                    name : "Артик",
+                    point : 4,
+                    extra : 1.25, 
+                    Ci : 1,
+                    win : 1/2
+                },
+                {
+                    name : "Крис",
+                    point : 0,
+                    extra : 0, 
+                    Ci : 0
+                }
+            ]
+        },
+        {
+            name : "Чур мы сверху",
+            players : [
+                {
+                    name : "Мортал",
+                    point : 1,
+                    extra : 0.3, 
+                    Ci : 0
+                },
+                {
+                    name : "Молчун",
+                    point : 3,
+                    extra : 0.2, 
+                    Ci : 1, 
+                    win : 1
+                }
+            ]
+        },
+        {
+            name : "Орел и решка",
+            players : [
+                {
+                    name : "Андерсен",
+                    point : 2,
+                    extra : 0, 
+                    Ci : 1,
+                    win : 0
+                },
+                {
+                    name : "Юстас",
+                    point : 1,
+                    extra : 0, 
+                    Ci : 0
+                }
+            ]
+        },
+        {
+            name : "Double Trouble",
+            players : [
+                {
+                    name : "Зодиак",
+                    point : 3,
+                    extra : 0.3, 
+                    Ci : 0
+                },
+                {
+                    name : "Дутче",
+                    point : 0,
+                    extra : 0, 
+                    Ci : 0
+                }
+            ]
+        },
+        {
+            name : "Привет Беларусь",
+            players : [
+                {
+                    name : "Альфа",
+                    point : 4,
+                    extra : 0.5, 
+                    Ci : 0
+                },
+                {
+                    name : "КО",
+                    point : 2,
+                    extra : 0, 
+                    Ci : 0
+                }
+            ]
+        },
+        {
+            name : "Играем в удовольствие",
+            players : [
+                {
+                    name : "Sha",
+                    point : 0,
+                    extra : 0, 
+                    Ci : 0
+                },
+                {
+                    name : "John Galt",
+                    point : 5,
+                    extra : 0.2, 
+                    Ci : 1,
+                    win : 1
+                }
+            ]
+        },
+        {
+            name : "Мяу",
+            players : [
+                {
+                    name : "Алиот",
+                    point : 1,
+                    extra : 0.2, 
+                    Ci : 0
+                },
+                {
+                    name : "Печаль",
+                    point : 0,
+                    extra : 0, 
+                    Ci : 0
+                },
+                {
+                    name : "Бон",
+                    point : 4,
+                    extra : 0.6, 
+                    Ci : 0
+                }
+            ]
+        }
+    ]
+}
+
+function getTable() {
+    return table_json;
+}
