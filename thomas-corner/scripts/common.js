62403ba892c9d41a516efce7e834238f9d177a3b
Nick Ezhenkov <ezhenkovnickolay@gmail.com>
2019-08-04 22:16:22 +0300

    [TG] Season 2 all stars final

--- thomas-corner/scripts/common.js
+++ thomas-corner/scripts/common.js
@@ -2,7 +2,7 @@ const season_rating_request_URL = 'https://x-maf-league.github.io/league-info/th
 const all_time_rating_request_URL = 'https://x-maf-league.github.io/league-info/thomas-corner/files/all_time_rating.json';
 
 const season_game_counter = 28;
-const total_game_counter = 47;
+const total_game_counter = 53;
 var init_game_counter;
 const update = '04.08';
 
