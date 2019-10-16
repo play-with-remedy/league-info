68e1fda5810d5abb7de87df75dd591c881a42aee
Nick Ezhenkov <ezhenkovnickolay@gmail.com>
2019-10-16 15:00:40 +0300

    [TR] test page demo 1

---
+++ thomas-corner/scripts/test.js
@@ -0,0 +1,49 @@
+
+function calculate() {
+    var array = $('textarea').val().split('"');
+    var result = [{}];
+    for(var i = 1; i < array.length; i = i + 2) {
+        if (array[i]) {
+            var list = array[i].split(' ').map(function(item) {
+                return item;
+            });
+            $.each(list, function(index, item) {
+                $.each(list, function(index, item2) {
+                    if (item !== item2) {
+                        if (!result[item]) {
+                            result[item] = {};
+                        }
+
+                        if (!result[item][item2]) {
+                            result[item][item2] = 1;
+                        } else {
+                            result[item][item2]++;
+                        }
+                    }
+                });
+            });
+        }
+    }
+
+    var table = "<table class='thomas_archive'><tr class='table_header'><td>#</td>";
+    for (var i=1; result[i]; i++) {
+        table += "<td>" + Object.keys(result)[i] + "</td>"
+    }
+    table += "</tr><tr>";
+
+    for (var i=1; result[i]; i++) {
+        table += "<td>" + Object.keys(result)[i] + "</td>";
+        for (var j=1; result[j]; j++) {
+            if(i===j) {
+                table += "<td style='color:red'>#</td>";
+            } else {
+                table += "<td>" + result[i][j] + "</td>";
+            }
+        }
+        table += "</tr>";
+    }
+
+    table += "</table>"
+
+    $('#result').empty().append(table);
+}
