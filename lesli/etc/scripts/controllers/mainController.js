5d285974fb6728cbca601da30633092456aa77de
Nick Ezhenkov <ezhenkovnickolay@gmail.com>
2021-12-04 04:57:47 +0300

    [VP] Graph and products tab was applied for 3 year files

--- lesli/etc/scripts/controllers/mainController.js
+++ lesli/etc/scripts/controllers/mainController.js
@@ -2,14 +2,8 @@ const fantasyApp = angular.module("fantasyApp", [])
 
 fantasyApp.controller("mainController", function ($scope, $q) {
   let companyObjectList = [];
-  let productObjectMap = {};
+  let productObjectList = [];
   let xslmObjects = [];
-
-  $scope.items = [
-    { id: 1, name: 'foo' },
-    { id: 2, name: 'bar' },
-    { id: 3, name: 'blah' }
-  ];
   
   $scope.init = function () {
     $scope.activeTab = 'company';
@@ -62,15 +56,12 @@ fantasyApp.controller("mainController", function ($scope, $q) {
 
 
   function buildProductObject(year) {
-    let productObjectList = [];
     let products = xslmObjects[year].find(object => object['дата отгр'] === 'ИТОГО');
     for (const key of Object.keys(products)) {
       if (key !== 'дата отгр') {
-        productObjectList.push({name: key, total: 0});
+        productObjectList.push({name: key, data: { [year]: { total: 0}}, total: 0});
       }
     }
-
-    productObjectMap[year] = productObjectList;
   }
 
   function buildCompanyObjectList(year) {
@@ -79,15 +70,24 @@ fantasyApp.controller("mainController", function ($scope, $q) {
       const date = otg_date ? otg_date.split(' ')[1] : undefined;
 
       for (const [key, value] of Object.entries(element)) {
-        let product = productObjectMap[year].find(object => object.name === key);
-        if (date && product) {
-          product[date] = product[date] ? product[date] + parseInt(value) : parseInt(value);
-          product.total = product.total + parseInt(value);
+        if (element['дата отгр'] !== 'ИТОГО') {
+          const total = parseFloat(value);
+          let product = productObjectList.find(object => object.name === key);
+          if (product) {
+            if (!product.data[year]) {
+              product.data[year] = {};
+              product.data[year].total = 0;
+            }
+
+            product.data[year][date] = product.data[year][date] ? round(product.data[year][date], total) : total;
+            product.data[year].total = round(product.data[year].total, total);
+            product.total = round(product.total, total);
+          }
         }
       }
 
       const company = Object.values(element)[1];
-      let total = parseInt(element['Итого']);
+      let total = parseFloat(element['Итого']);
       let sameElement = companyObjectList.find(object => object.name === company);
 
       if (sameElement) {
@@ -95,9 +95,9 @@ fantasyApp.controller("mainController", function ($scope, $q) {
           sameElement.data[year] = {};
           sameElement.data[year].total = 0;
         }
-        sameElement.data[year][date] = sameElement.data[year][date] ? sameElement.data[year][date] + total : total;
-        sameElement.data[year].total += total;
-        sameElement.total += total;
+        sameElement.data[year][date] = sameElement.data[year][date] ? round(sameElement.data[year][date], total) : total;
+        sameElement.data[year].total = round(sameElement.data[year].total, total);
+        sameElement.total = round(sameElement.total, total);
       }
 
       if (company && total && date && !sameElement) {
@@ -107,19 +107,33 @@ fantasyApp.controller("mainController", function ($scope, $q) {
   }
 
   $scope.setYear = function (year) {
+    $scope.isLoaded = false;
     $scope.activeYear = year;
+
+    if (year !== 3) {
+      if ($scope.activeTab === 'stats') {
+        $scope.showStats();
+        $scope.isLoaded = true;
+      }
+    }
     
-    if(year === 3) {
+    if (year === 3) {
       if (Object.keys(xslmObjects).length !== 3) {
-        $scope.isLoaded = false;
         sendRequestByUrl('2020').then(function success(response) {
           if (response) {
             sendRequestByUrl('2019').then(function success(response) {
+              if ($scope.activeTab === 'stats') {
+                $scope.showStats();
+              }
               $scope.isLoaded = response;
-              $scope.showCompany();
             });
           } 
         });
+      } else {
+        if ($scope.activeTab === 'stats') {
+          $scope.showStats();
+          $scope.isLoaded = true;
+        }
       }
     }
   }
@@ -157,12 +171,12 @@ fantasyApp.controller("mainController", function ($scope, $q) {
     $scope.currentOrderYear = year;
 
     if ($scope.activeTab === 'product' || $scope.activeTab === 'topProduct') {
-      productObjectMap['2021'].sort(function (a, b) {
+      productObjectList.sort(function (a, b) {
         return sort(a, b);
       });
     } else if ($scope.activeTab === 'company' || $scope.activeTab === 'topCompany') {
       companyObjectList.sort(function (a, b) {
-          return sort(a, b);
+        return sort(a, b);
       });
     }
 
@@ -187,44 +201,85 @@ fantasyApp.controller("mainController", function ($scope, $q) {
 
   $scope.showStats = function () {
     $scope.activeTab = 'stats';
-    let productObjectList = productObjectMap['2021'];
     
     google.charts.load('current', {'packages':['corechart']});
     google.charts.setOnLoadCallback(drawChart);
 
     function drawChart() {
-      var data = google.visualization.arrayToDataTable([
-        ['Месяц', 'Товар, кг'],
-        ['Jan',  $scope.sum(productObjectList, 'Jan')],
-        ['Feb',  $scope.sum(productObjectList, 'Feb')],
-        ['Mar',  $scope.sum(productObjectList, 'Mar')],
-        ['Apr',  $scope.sum(productObjectList, 'Apr')],
-        ['May',  $scope.sum(productObjectList, 'May')],
-        ['Jun',  $scope.sum(productObjectList, 'Jun')],
-        ['Jul',  $scope.sum(productObjectList, 'Jul')],
-        ['Aug',  $scope.sum(productObjectList, 'Aug')],
-        ['Sep',  $scope.sum(productObjectList, 'Sep')],
-        ['Oct',  $scope.sum(productObjectList, 'Oct')],
-        ['Now',  $scope.sum(productObjectList, 'Nov')],
-        ['Dec',  $scope.sum(productObjectList, 'Dec')],
-      ]);
+      let chartData = [
+        ['Месяц', 'Товар, кг']
+      ]
 
-      var options = {
-        title: 'Company Performance',
+      let chartDataArray2021 = [
+        ['Jan',  sum(productObjectList, 'Jan', '2021')],
+        ['',  sum(productObjectList, 'Feb', '2021')],
+        ['Mar',  sum(productObjectList, 'Mar', '2021')],
+        ['',  sum(productObjectList, 'Apr', '2021')],
+        ['May',  sum(productObjectList, 'May', '2021')],
+        ['2021',  sum(productObjectList, 'Jun', '2021')],
+        ['Jul',  sum(productObjectList, 'Jul', '2021')],
+        ['',  sum(productObjectList, 'Aug', '2021')],
+        ['Sep',  sum(productObjectList, 'Sep', '2021')],
+        ['',  sum(productObjectList, 'Oct', '2021')],
+        ['Nov',  sum(productObjectList, 'Nov', '2021')],
+        ['',  sum(productObjectList, 'Dec', '2021')]
+      ];
+
+      let chartDataArray20192020 = [
+        ['Jan', sum(productObjectList, 'Jan', '2019')],
+        ['', sum(productObjectList, 'Feb', '2019')],
+        ['Mar',  sum(productObjectList, 'Mar', '2019')],
+        ['',  sum(productObjectList, 'Apr', '2019')],
+        ['May',  sum(productObjectList, 'May', '2019')],
+        ['2019',  sum(productObjectList, 'Jun', '2019')],
+        ['Jul',  sum(productObjectList, 'Jul', '2019')],
+        ['',  sum(productObjectList, 'Aug', '2019')],
+        ['Sep',  sum(productObjectList, 'Sep', '2019')],
+        ['',  sum(productObjectList, 'Oct', '2019')],
+        ['Nov',  sum(productObjectList, 'Nov', '2019')],
+        ['',  sum(productObjectList, 'Dec', '2019')],
+        ['Jan',  sum(productObjectList, 'Jan', '2020')],
+        ['',  sum(productObjectList, 'Feb', '2020')],
+        ['Mar',  sum(productObjectList, 'Mar', '2020')],
+        ['',  sum(productObjectList, 'Apr', '2020')],
+        ['May',  sum(productObjectList, 'May', '2020')],
+        ['2020',  sum(productObjectList, 'Jun', '2020')],
+        ['Jul',  sum(productObjectList, 'Jul', '2020')],
+        ['',  sum(productObjectList, 'Aug', '2020')],
+        ['Sep',  sum(productObjectList, 'Sep', '2020')],
+        ['',  sum(productObjectList, 'Oct', '2020')],
+        ['Nov',  sum(productObjectList, 'Nov', '2020')],
+        ['',  sum(productObjectList, 'Dec', '2020')]
+      ];
+
+      if ($scope.activeYear === 3) {
+        chartDataArray2021 = chartDataArray20192020.concat(chartDataArray2021);
+      }
+
+      chartData = chartData.concat(chartDataArray2021);
+      const data = google.visualization.arrayToDataTable(chartData);
+
+      const options = {
+        title: 'Статистика продаж',
         curveType: 'line',
-        legend: { position: 'bottom' }
+        legend: { position: 'bottom' },
       };
 
-      var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+      const chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
 
       chart.draw(data, options);
     }
   };
 
-  $scope.sum = function(items, prop){
-    return items.reduce( function(a, b) {
-        const t = b[prop] ? b[prop] : 0;
-        return a + t;
+  function sum(items, prop, year){
+    return items.reduce(function(a, b) {
+        const t = b.data[year] && b.data[year][prop] ? b.data[year][prop] : 0;
+        return round(a, t);
     }, 0);
   };
+
+  function round(firstValue, secondValue) {
+    if (!secondValue) secondValue = 0;
+    return Math.round((firstValue + secondValue) * 10) / 10;
+  }
 });
