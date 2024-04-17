05d5968c0b3ee9b9d11e3aff6ad335f82de8da44
Remedy <nickolayezhenkov@gmail.com>
2024-04-18 01:16:03 +0300

    [S2] Sakura Fantasy table was created

---
+++ sakura/etc/scripts/fantasy-table.js
@@ -0,0 +1,135 @@
+const data = {
+    "Архи": [
+        "Архи", "Gadfour", "Грибочки", "Джейн",
+    ],
+    "Флэш": [
+        "Интерактив", "Весна", "Бестия", "Карась",
+    ],
+    "Марко Ройс": [
+        "Интерактив", "Злая Киса", "Rocket Woman", "Марго",
+    ],
+    "SCHOKK": [
+        "Интерактив", "Саранча", "Злая Киса", "Манекен",
+    ],
+    "Карась": [
+        "Артик", "Боня", "Манекен", "Карась",
+    ],
+    "ППК": [
+        "Интерактив", "Morti", "Gadfour", "Манекен",
+    ],
+    "Morti": [
+        "Архи", "Morti", "Rocket Woman", "Карась",
+    ],
+    //"Есаул": [
+    //    "Артик", "Gadfour", "Злая Киса", "Джейн",
+    //],
+    "Марго": [
+        "Боня", "Пчела", "Марго", "Джейн",
+    ],
+    "Лекса": [
+        "SOVEST", "Скарлетт", "Спонжик", "Манекен",
+    ],
+    "Скарлетт": [
+        "Интерактив", "Скарлетт", "Джейн", "Манекен",
+    ],
+    "Пчела": [
+        "Интерактив", "Пчела", "Cherry Pick", "Манекен",
+    ],
+    "Бендер": [
+        "Интерактив", "Саранча", "Марго", "Манекен",
+    ],
+    "Юрия": [
+        "Юрия", "Мориарти", "Спонжик", "Марго",
+    ],
+    "SOVEST": [
+        "SOVEST", "Лекса", "Злая Киса", "Бестия",
+    ],
+    "Сергеевна": [
+        "Сергеевна", "Бестия", "Грибочки", "Карась",
+    ],
+    "Манекен": [
+        "Интерактив", "Бестия", "Грибочки", "Манекен",
+    ],
+    "Весна": [
+        "Артик", "Весна", "Бестия", "Манекен",
+    ],
+    "Джейн": [
+        "SOVEST", "Пчела", "Rocket Woman", "Джейн",
+    ],
+    "Бестия": [
+        "Сергеевна", "Саранча", "Бестия", "Манекен",
+    ],
+    "Michael": [
+        "Интерактив", "Michael", "Джейн", "Манекен",
+    ],
+    //"Хаус": [
+    //    "Сергеевна", "Саранча", "Gadfour", "Манекен",
+    "Rocket Woman": [
+        "Артик", "Саранча", "Джейн", "Rocket Woman",
+    ],
+    "Cherry Pick": [
+        "Интерактив", "Morti", "Cherry Pick", "Rocket Woman",
+    ],
+    "Lina": [
+        "Интерактив", "Lina", "Спонжик", "Манекен",
+    ],
+    "Young": [
+        "Young", "Пчела", "Злая Киса", "Cherry Pick",
+    ],
+    "Лексус": [
+        "Боня", "Morti", "Злая Киса", "Бестия",
+    ],
+    "Yuliya": [
+        "Интерактив", "Morti", "Лекса", "Манекен",
+    ],
+    "Curly": [
+        "Сергеевна", "Пчела", "Curly", "Джейн",
+    ],
+    "Грибочки": [
+        "Сергеевна", "Скарлетт", "Грибочки", "Манекен",
+    ],
+    "Спонжик": [
+        "Интерактив", "Gadfour", "Спонжик", "Джейн",
+    ],
+    "Артик": [
+        "Артик", "Саранча", "Бестия", "Карась",
+    ],
+    "Злая Киса": [
+        "Интерактив", "Злая Киса", "Бестия", "Манекен",
+    ],
+    "Мориарти": [
+        "Юрия", "Мориарти", "Спонжик", "Злая Киса",
+    ],
+    "Самаритянка": [
+        "Интерактив", "Мориарти", "Бестия", "Карась",
+    ],
+};
+
+$(document).ready(function() {
+    let table = "<table>";
+    table += "<tr><th>#</th><th>Player</th><th colspan='4'>Team</th><th>Sum</th><th>Avg</th></tr>";
+    Object.keys(data).forEach((fpsPlayerName, index) => {
+        table += "<tr><td>" + (index + 1) + "</td><td class='player-name'>" + fpsPlayerName + "</td>"
+        let totalScore = 0;
+        let totalTours = 0;
+        data[fpsPlayerName].forEach(player => {
+            const playerScore = tableData[player].score;
+            const playerTours = tableData[player].tours;
+            const playerImg = tableData[player].img;
+            table += "<td><div class='main-fanstasy-table-player-img' style='background-image: url(" + playerImg + ")'></div>" 
+                    + "<p>" + playerScore + "</p></td>";
+            totalScore += playerScore;
+            totalTours += playerTours;
+        });
+        table += "<td class='score'>" + totalScore + "</td>";
+        if (totalTours !== 0) {
+            table += "<td class='score'>" + (totalScore / totalTours).toFixed(2) + "</td>";
+        } else {
+            table += "<td class='score'>0</td>";
+        }
+        table += "</tr>";
+    });
+    table += "</table>"
+    $('.main-fanstasy-table').append(table);
+
+});
---
+++ sakura/etc/scripts/sakura-table.js
@@ -0,0 +1,153 @@
+const tableData = {
+    "Артик": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Artik.jpeg",
+    },
+    "Архи": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Arhi.jpeg",
+    },
+    "Интерактив": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Inter.jpeg",
+    },
+    "Сергеевна": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Sergeevna.jpeg",
+    },
+    "Боня": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Bonya.jpeg",
+    },
+    "SOVEST": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Sovest.jpeg",
+    },
+    "Жнец": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Jnec.jpeg",
+    },
+    "Young": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Young.jpeg",
+    },
+    "Юрия": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Yria.jpeg",
+    },
+    "Скарлетт": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Skarlet.jpeg",
+    },
+    "Пчела": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Pcela.jpeg",
+    },
+    "Мориарти": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Moriarti.jpeg",
+    },
+    "Саранча": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Sarancha.jpeg",
+    },
+    "Весна": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Vesna.jpeg",
+    },
+    "Michael": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Michael.jpeg",
+    },
+    "Gadfour": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Gadfour.jpeg",
+    },
+    "Лекса": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Leksa.jpeg",
+    },
+    "Lina": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Lina.jpeg",
+    },
+    "Morti": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Morti.jpeg",
+    },
+    "Злая Киса": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/ZK.jpeg",
+    },
+    "Марго": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Margo.jpeg",
+    },
+    "Спонжик": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Sponjik.jpeg",
+    },
+    "Бестия": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Bestia.jpeg",
+    },
+    "Cherry Pick": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Cherry.png",
+    },
+    "Грибочки": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Gribochki.jpeg",
+    },
+    "Rocket Woman": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Roket.jpeg",
+    },
+    "Джейн": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Jane.jpeg",
+    },
+    "Curly": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Curly.jpeg",
+    },
+    "Манекен": {
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Maneken.jpeg",
+    },
+    "Карась": {
+        nickname: "Карась",
+        tours: 0,
+        score: 0,
+        img: "../etc/images/players/Karas.jpeg",
+    },
+};
