d71748dc6df925ee601df2d3638eddd8223ce86d
Remedy <nickolayezhenkov@gmail.com>
2024-09-15 11:53:18 +0300

    [C] Update fonst for timetable

--- consigliere/scripts/timetable.js
+++ consigliere/scripts/timetable.js
@@ -69,18 +69,34 @@ const teams = [
 ];
 
 $(document).ready(function() {
-    let timetable = new Array(8);
+    // let timetable = new Array(8);
 
-    for (let i = 0; i < timetable.length; i++) {
-        timetable[i] = new Array(10);
-      }
+    // for (let i = 0; i < timetable.length; i++) {
+    //     timetable[i] = new Array(10);
+    //   }
 
-    teams.forEach((team) => {
-        team.slots.forEach((slot, index) => {
-            timetable[index][slot] = team.teamName;
-        });
-    })
+    // teams.forEach((team) => {
+    //     team.slots.forEach((slot, index) => {
+    //         timetable[index][slot] = team.teamName;
+    //     });
+    // })
 
-    console.log(timetable);
+    // console.log(timetable);
+
+    let timetable = "";
+    for(let i = 0; i < 8; i++) {
+        const number = i + 1;
+        timetable += "<div id='game-" + number + "' class='game'>";
+        timetable += "<p class='game-number'>Игра " + number + "</p>";
+        timetable += "<div class='mafia-table-wrapper'>";
+        for (let j = 0; j < 3; j++) {
+            timetable += "<div class='mafia-table'>";
+            timetable += "<p>Стол " + (j + 1) + "</p><table>"
+            timetable += "</table></div>";
+        }
+        timetable += "</div></div>";
+    }
+
+    $('#timetable').append(timetable);
 
 });
